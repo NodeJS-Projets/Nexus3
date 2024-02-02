85ce454145dabb69b91097a17835f75f1adc6600
Ashik Rai <ashik.rai@lokibots.com>
2024-02-02 15:10:16 +0530

    code restructured

---
+++ src/controllers/LinkedInController.js
@@ -0,0 +1,14 @@
+exports.LinkedInController = {
+    getUserData: (req, res) => {
+        res.send("LinkedIn Controller Data: Ashik Rai")
+    }
+}
+
+
+// const LinkedInController = {
+//     getUserData: (req, res) => {
+//         res.send("LinkedIn Controller: Ashik Rai")
+//     }
+// };
+
+// export {LinkedInController};
--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ const express= require("express");
 // Env imports
 require('dotenv').config()
 
+// Importing Controllers
+const {LinkedInController} = require("./controllers/LinkedInController.js");
+// import { LinkedInController } from "./controllers/linkedInController";
+
+
 // For handling the api calls
 const app= express();
 
@@ -11,11 +16,7 @@ const app= express();
 app.get("/", (req, res) => {
     res.send("Sairam")
 });
-
-app.get("/my-linkedin", (req, res) => {
-    res.send("LinkedIn: Ashik Rai")
-});
-
+app.get("/my-linkedin", LinkedInController.getUserData);
 
 app.listen(process.env.PORT, () => {
     console.log(`Port is ${process.env.PORT}`);
